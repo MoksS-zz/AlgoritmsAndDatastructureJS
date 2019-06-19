55306aa1c5fe157421dea3547877df495f5402cf
maksim <skarobogyodko@gmail.com>
2019-06-20 00:48:16 +0300

    add exemple dinamic programming

--- babelsort.js
+++ babelsort.js
@@ -1,22 +1,23 @@
-"use strict";
-function sortBubble(data) {
-    var tmp; 
-    const length = data.length;
-    for (let i = length - 1; i > 0; i--) {  
-        var counter=0;
-        for (var j = 0; j < i-1; j++) {
-            if (data[j] > data[j+1]) {
-                tmp = data[j];
-                data[j] = data[j+1];
-                data[j+1] = tmp;
-                counter++;
+'use strict'
+const c = 41
+function sortBubble (data) {
+    let tmp
+    const length = data.length
+    for (let i = length - 1; i > 0; i--) {
+        let counter = 0
+        for (let j = 0; j < i - 1; j++) {
+            if (data[j] > data[j + 1]) {
+                tmp = data[j]
+                data[j] = data[j + 1]
+                data[j + 1] = tmp
+                counter++
             }
-        }  
-        if(counter==0){
-          break;
-        } 
+        }
+        if (counter === 0) {
+            break
+        }
     }
-  return data;
- };
-
- console.log(sortBubble([1,2,3,4,67,324,765,234,45,123,123,124,45,6,324,234,]));
+    return data
+};
+console.log(c)
+console.log(sortBubble([1, 2, 3, 4, 67, 324, 765, 234, 45, 123, 123, 124, 45, 6, 324, 234]))
