5afd5936b70e98f19094ec92077457ddc5dae9ae
MoksS <MoksShuv@yandex.ru>
2020-04-03 01:25:54 +0300

    изменено:      algoritm/binaryAlgoritm/binarySearch.js 	изменено:      algoritm/sort/QuickSort.js

--- algoritm/binaryAlgoritm/binarySearch.js
+++ algoritm/binaryAlgoritm/binarySearch.js
@@ -1,13 +1,13 @@
 "use strict";
-function binSearch (arr,item){
+Array.prototype.binSearch = function (item) {
     const low = 0;
-    let high = arr.length - 1;
+    let high = this.length - 1;
     let mid,guess;
 
     while(low <= high){
     
         mid = Math.floor((low + high)/2);
-        guess = arr[mid]
+        guess = this[mid]
         if (guess == item) return mid;
         guess > item ? high = mid - 1 : low = mid + 1;
     }
@@ -17,4 +17,4 @@ function binSearch (arr,item){
 
 const list = [1,3, 5, 7, 9];
 
-console.log(binSearch(list,5));
+console.log(list.binSearch(5));
